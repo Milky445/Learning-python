33c464ac3e134e621d23ac69085c23b6079c1d1b
Santi <106703878+Milky445@users.noreply.github.com>
2022-09-12 09:27:15 -0400

    Add files via upload

---
+++ favorite_number.py
@@ -0,0 +1,2 @@
+fav_number = "13"
+print("My favourite number is: {}".format(fav_number))
---
+++ name_cases.py
@@ -0,0 +1,9 @@
+mom_name = "Georgia"
+print("Hello {}, would you like to learn some Python today?".format(mom_name))
+
+famous_person = "Albert Einstein"
+message = '"A person who never made a mistake never tried anything new."'
+print("{} once said, {}".format(famous_person, message))
+
+name = "   \n\tSanti   "
+print(name.strip())
