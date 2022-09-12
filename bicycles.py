2486c01ccbd92eb16051d1b9726dbfd2021edf48
Santi <106703878+Milky445@users.noreply.github.com>
2022-09-12 09:56:59 -0400

    Add files via upload

---
+++ bicycles.py
@@ -0,0 +1,11 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "My first bicycle was a {}".format(bicycles[0])
+print(message)
+
+
+
+names = ['Tyler', 'Bob', 'Kevin', 'Jamal']
+print("Hello, {}".format(names[0]))
+print("Hello, {}".format(names[1]))
+print("Hello, {}".format(names[2]))
+print("Hello, {}".format(names[3]))
